68c16ade974f26e875d67eba58071d1fdf5c1e2a
Camila Stefano <cstefano@makingsense.com.ar>
2021-12-07 10:49:55 -0300

    add hover in btn and update favicon

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
+import "../Button.css";
 const styles = {
   button: {
-    backgroundColor: "#33AD73",
     color: "#FFF",
     padding: "15px 20px",
     border: "none",
@@ -12,7 +12,7 @@ const styles = {
 
 class Button extends Component {
   render() {
-    return <button style={styles.button} {...this.props} />; //con el spread operator le pasamos las propiedades del componente Button, en este caso el onClick
+    return <button className="buttonAddtoCart" style={styles.button} {...this.props} />; //con el spread operator le pasamos las propiedades del componente Button, en este caso el onClick
   }
 }
 
